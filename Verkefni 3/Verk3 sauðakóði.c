fab0f90580c3943d58804bd1cc735c3409fa0d90
olipeturolsen98 <oliolsen@outlook.com>
2017-10-23 12:10:01 +0000

    Add files via upload

---
+++ Verkefni 3/Verk3 sauðakóði.c
@@ -0,0 +1,16 @@
+if(vexRT[Btn5D] == 1)
+{
+	   motor[armMotor] = 60;
+}
+if(vexRT[Btn5U] == 1)
+{
+		motor[armMotor] = -60;
+}
+if(vexRT[Btn6U] == 1)
+{
+	motor[clawMotor] = 60;
+}
+if(vexRT[Btn6D] == 1)
+{
+	motor[clawMotor] = -60;
+}

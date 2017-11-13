a05c1f9bc41302712d7dbfc3bc6c06a0dc49e4d4
olipeturolsen98 <oliolsen@outlook.com>
2017-11-13 10:41:31 +0000

    verkefni 4 kóði

---
+++ Verkefni 4/Verk4 sauðakóði.c
@@ -0,0 +1,25 @@
+﻿while(Btn7u && pushBtn == 0)
+{
+  if(stökt á ljósum){
+  driveForward()
+    if(SonarSkynjarHættu)
+    {
+      left,right motor = 0
+      left,right motor -127
+      Bíða í 200ms
+      right motor = 127
+      left motor = -127
+    }
+  }
+}
+betra svona
+while(Btn7u || pushBtn == 0)
+{
+  if(lights_on){
+  driveForward()
+    if(proximity)
+    {
+      turn()
+    }
+  }
+}
